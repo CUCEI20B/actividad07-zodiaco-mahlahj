9d942b167ad35f2132c7fe3f17958e69e983f267
Guillermo G. Kelly S <malak_ai@hotmail.com>
2020-09-24 08:47:31 -0500

    prueba01

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def signos():
         if ((int(mes)==12 and int(dia) >= 22)or(int(mes)==1 and int(dia)<= 19)):
                 signo_zodiacal = ("capricornio")
         elif ((int(mes)==1 and int(dia) >= 20)or(int(mes)==2 and int(dia)<= 17)):
-                signo_zodiacal = ("acuario")
+                signo_zodiacal = ("sagitario")
         elif ((int(mes)==2 and int(dia) >= 18)or(int(mes)==3 and int(dia)<= 19)):
                 signo_zodiacal = ("piscis")
         elif ((int(mes)==3 and int(dia) >= 20)or(int(mes)==4 and int(dia)<= 19)):
@@ -19,13 +19,13 @@ def signos():
         elif ((int(mes)==7 and int(dia) >= 23)or(int(mes)==8 and int(dia)<= 22)): 
                 signo_zodiacal = ("leo")
         elif ((int(mes)==8 and int(dia) >= 23)or(int(mes)==9 and int(dia)<= 22)): 
-                signo_zodiacal = ("virgo")
+                signo_zodiacal = ("geminis")
         elif ((int(mes)==9 and int(dia) >= 23)or(int(mes)==10 and int(dia)<= 22)):
                 signo_zodiacal = ("libra")
         elif ((int(mes)==10 and int(dia) >= 23)or(int(mes)==11 and int(dia)<= 21)): 
                 signo_zodiacal = ("escorpio")
         elif ((int(mes)==11 and int(dia) >= 22)or(int(mes)==12 and int(dia)<= 21)):
-                signo_zodiacal = ("sagitario")
+                signo_zodiacal = ("virgo")
 
         print(signo_zodiacal)
         
